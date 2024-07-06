6be0c6f63e713ce6998d28b13cc47cdc7383ecb9
chini <chinmayasharma@proton.me>
2024-07-06 17:57:14 +0530

    about me changed

--- src/app/common/translations.ts
+++ src/app/common/translations.ts
@@ -7,7 +7,7 @@ export const translations = {
   mario: "mario",
   download: "download",
   getToKnowMe: "get to know me",
-  aboutMe: `I'm Chinmaya, a passionate web developer and tech enthusiast based in Gurgaon. With a degree in computer science from Amity School of Engineering and Technology, I excel in creating vibrant, accessible web designs.
+  aboutMe: `Hi, I'm Chinmaya a passionate web developer and tech enthusiast based in Gurgaon. With a degree in computer science from Amity School of Engineering and Technology, I excel in creating vibrant, accessible web designs.
 As a Software Engineer at SHL India, I've worked on projects like the TC Plus assessments platform and avatar-based assessments. I'm proficient with Angular and React and adept at picking up new frameworks.
 One highlight is developing a gamified process monitoring assessment, integrating advanced animations. For my work, I received the Experienced Practitioner award in October 2022.
 I thrive both in teams and independently, mentoring junior members and continuously exploring new technologies to enhance user experiences. Let's connect and build something amazing together!
--- src/app/components/info.tsx
+++ src/app/components/info.tsx
@@ -1,66 +1,104 @@
-import { FC, memo, useState } from "react";
+import { FC, memo, useEffect, useMemo, useState } from "react";
 import UpperCase from "./upperCase";
 import { useLocalization } from "../common/localization";
+import { Editor } from "@monaco-editor/react";
+import { animationFrameScheduler, asyncScheduler, Subscription } from "rxjs";
+import { getRandomNumberBetween } from "../common/jsUtils";
 
-function NameC() {
+// function NameC() {
+//   const { translations } = useLocalization();
+
+//   return (
+//     <div
+//       className={`flex font-mono text-5xl flex-1 select-none items-center dark:text-MANNA-400 text-YOYO-600`}
+//     >
+//       <UpperCase>{translations.chinmaya}</UpperCase>
+//     </div>
+//   );
+// }
+
+// const Name = memo(NameC);
+
+// function AboutMeC() {
+//   const { translations } = useLocalization();
+
+//   return (
+//     <div
+//       className={`flex font-mono lg:text-xl text-base md:text-md flex-1 select-none items-center dark:text-MANNA-400 text-YOYO-600`}
+//     >
+//       {translations.aboutMe}
+//     </div>
+//   );
+// }
+
+// const AboutMe = memo(AboutMeC);
+
+// const VIEW_STATES: Array<FC> = [Name, AboutMe];
+
+// const Info: FC = () => {
+//   const [viewIndex, setViewIndex] = useState<number>(0);
+
+//   return (
+//     <div className={`pl-12 flex flex-col h-full lg:w-4/12 w-3/4`}>
+//       {VIEW_STATES.map((Component, index) =>
+//         index === viewIndex ? <Component key={index} /> : null
+//       )}
+
+//       <div
+//         className={`pb-4 font-mono text-4xl font-extrabold flex justify-between`}
+//       >
+//         <button
+//           onClick={() => setViewIndex(viewIndex - 1)}
+//           className={`${viewIndex === 0 ? "invisible" : ""}`}
+//         >
+//           {"<"}
+//         </button>
+//         <button
+//           onClick={() => setViewIndex(viewIndex + 1)}
+//           className={`${
+//             viewIndex === VIEW_STATES.length - 1 ? "invisible" : ""
+//           }`}
+//         >
+//           {">"}
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default memo(Info);
+
+function Info() {
   const { translations } = useLocalization();
+  const [editorVal, setEditorVal] = useState<string>("");
+
+  const INFO_TXT = useMemo(() => {
+    return translations.aboutMe;
+  }, []);
+
+  useEffect(() => {
+    if (editorVal === INFO_TXT) return;
+
+    const delay = getRandomNumberBetween(30, 100);
+
+    const scheduleSub = asyncScheduler.schedule(() => {
+      const nextVal = editorVal + INFO_TXT[editorVal.length];
+
+      setEditorVal(nextVal);
+    }, delay);
+
+    return () => {
+      scheduleSub.unsubscribe();
+    };
+  }, [editorVal]);
 
   return (
-    <div
-      className={`flex font-mono text-5xl flex-1 select-none items-center dark:text-MANNA-400 text-YOYO-600`}
-    >
-      <UpperCase>{translations.chinmaya}</UpperCase>
-    </div>
-  );
-}
-
-const Name = memo(NameC);
-
-function AboutMeC() {
-  const { translations } = useLocalization();
-
-  return (
-    <div
-      className={`flex font-mono lg:text-xl text-base md:text-md flex-1 select-none items-center dark:text-MANNA-400 text-YOYO-600`}
-    >
-      {translations.aboutMe}
-    </div>
-  );
-}
-
-const AboutMe = memo(AboutMeC);
-
-const VIEW_STATES: Array<FC> = [Name, AboutMe];
-
-const Info: FC = () => {
-  const [viewIndex, setViewIndex] = useState<number>(0);
-
-  return (
-    <div className={`pl-12 flex flex-col h-full lg:w-4/12 w-3/4`}>
-      {VIEW_STATES.map((Component, index) =>
-        index === viewIndex ? <Component key={index} /> : null
-      )}
-
-      <div
-        className={`pb-4 font-mono text-4xl font-extrabold flex justify-between`}
-      >
-        <button
-          onClick={() => setViewIndex(viewIndex - 1)}
-          className={`${viewIndex === 0 ? "invisible" : ""}`}
-        >
-          {"<"}
-        </button>
-        <button
-          onClick={() => setViewIndex(viewIndex + 1)}
-          className={`${
-            viewIndex === VIEW_STATES.length - 1 ? "invisible" : ""
-          }`}
-        >
-          {">"}
-        </button>
+    <div className="flex dark:text-teal-400 font-bold font-mono items-center text-YOYO-600 h-full lg:w-4/12 text-xl py-6 px-12 opacity-60 bg-POP_BLACK-300">
+      <div className="h-full w-full overflow-y-auto items-center">
+        {editorVal}
       </div>
     </div>
   );
-};
+}
 
 export default memo(Info);

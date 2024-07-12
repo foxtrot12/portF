2b7f49b4c0f9870298cb44d915807277ad1f3623
chini <chinmayasharma@proton.me>
2024-07-13 03:01:06 +0530

    skill enhancement

--- src/app/components/orbit.tsx
+++ src/app/components/orbit.tsx
@@ -13,73 +13,73 @@ function Orbit() {
   const contRef = useRef<HTMLDivElement>(null);
 
   const wSize = useWindowSize();
-  const orbs: any[] = [];
 
   const rand = (rMi: number, rMa: number) =>
     Math.floor(Math.random() * (rMa - rMi + 1) + rMi);
 
-  const createOrb = (
-    mx: number,
-    my: number,
-    cw: number,
-    ch: number,
-    ctx: CanvasRenderingContext2D
-  ) => {
-    const dx = cw / 2 - mx;
-    const dy = ch / 2 - my;
-    const dist = Math.sqrt(dx * dx + dy * dy);
-    const angle = Math.atan2(dy, dx);
-    orbs.push({
-      x: mx,
-      y: my,
-      lastX: mx,
-      lastY: my,
-      hue: 0,
-      colorAngle: 0,
-      angle: angle + Math.PI / 2,
-      size: rand(1, 3) / 2,
-      centerX: cw / 2,
-      centerY: ch / 2,
-      radius: dist,
-      speed: (rand(5, 10) / 1000) * (dist / 750) + 0.015,
-      alpha: 1 - Math.abs(dist) / cw,
-      draw: function () {
-        ctx.strokeStyle = `hsla(${this.colorAngle}, 100%, 50%, 1)`;
-        ctx.lineWidth = this.size;
-        ctx.beginPath();
-        ctx.moveTo(this.lastX, this.lastY);
-        ctx.lineTo(this.x, this.y);
-        ctx.stroke();
-      },
-      update: function (cw: number, ch: number) {
-        this.lastX = this.x;
-        this.lastY = this.y;
-        const x1 = cw / 2;
-        const y1 = ch / 2;
-        const x2 = this.x;
-        const y2 = this.y;
-        const rise = y1 - y2;
-        const run = x1 - x2;
-        const slope = -(rise / run);
-        let radian = Math.atan(slope);
-        let angleH = Math.floor(radian * (180 / Math.PI));
-        if (x2 < x1 && y2 < y1) angleH += 180;
-        if (x2 < x1 && y2 > y1) angleH += 180;
-        if (x2 > x1 && y2 > y1) angleH += 360;
-        if (y2 < y1 && slope === -Infinity) angleH = 90;
-        if (y2 > y1 && slope === Infinity) angleH = 270;
-        if (x2 < x1 && slope === 0) angleH = 180;
-        if (isNaN(angleH)) angleH = 0;
-
-        this.colorAngle = angleH;
-        this.x = this.centerX + Math.sin(this.angle * -1) * this.radius;
-        this.y = this.centerY + Math.cos(this.angle * -1) * this.radius;
-        this.angle += this.speed;
-      },
-    });
-  };
-
   useEffect(() => {
+    const orbs: any[] = [];
+
+    const createOrb = (
+      mx: number,
+      my: number,
+      cw: number,
+      ch: number,
+      ctx: CanvasRenderingContext2D
+    ) => {
+      const dx = cw / 2 - mx;
+      const dy = ch / 2 - my;
+      const dist = Math.sqrt(dx * dx + dy * dy);
+      const angle = Math.atan2(dy, dx);
+      orbs.push({
+        x: mx,
+        y: my,
+        lastX: mx,
+        lastY: my,
+        hue: 0,
+        colorAngle: 0,
+        angle: angle + Math.PI / 2,
+        size: rand(1, 3) / 2,
+        centerX: cw / 2,
+        centerY: ch / 2,
+        radius: dist,
+        speed: (rand(5, 10) / 1000) * (dist / 750) + 0.015,
+        alpha: 1 - Math.abs(dist) / cw,
+        draw: function () {
+          ctx.strokeStyle = `hsla(${this.colorAngle}, 100%, 50%, 1)`;
+          ctx.lineWidth = this.size;
+          ctx.beginPath();
+          ctx.moveTo(this.lastX, this.lastY);
+          ctx.lineTo(this.x, this.y);
+          ctx.stroke();
+        },
+        update: function (cw: number, ch: number) {
+          this.lastX = this.x;
+          this.lastY = this.y;
+          const x1 = cw / 2;
+          const y1 = ch / 2;
+          const x2 = this.x;
+          const y2 = this.y;
+          const rise = y1 - y2;
+          const run = x1 - x2;
+          const slope = -(rise / run);
+          let radian = Math.atan(slope);
+          let angleH = Math.floor(radian * (180 / Math.PI));
+          if (x2 < x1 && y2 < y1) angleH += 180;
+          if (x2 < x1 && y2 > y1) angleH += 180;
+          if (x2 > x1 && y2 > y1) angleH += 360;
+          if (y2 < y1 && slope === -Infinity) angleH = 90;
+          if (y2 > y1 && slope === Infinity) angleH = 270;
+          if (x2 < x1 && slope === 0) angleH = 180;
+          if (isNaN(angleH)) angleH = 0;
+
+          this.colorAngle = angleH;
+          this.x = this.centerX + Math.sin(this.angle * -1) * this.radius;
+          this.y = this.centerY + Math.cos(this.angle * -1) * this.radius;
+          this.angle += this.speed;
+        },
+      });
+    };
     const subsManager = new Subscription();
 
     const canvas = canvasRef.current;
